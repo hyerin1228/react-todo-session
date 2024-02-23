df36670bd0202f8a328587ab4920029a02028197
hyerin1228 <hyerin1228@gmail.com>
2024-02-23 22:52:47 +0900

    #2. todos.context 로 todos 를 homepage 컴포넌트로부터 분리. :)

---
+++ src/contexts/todos.context.tsx
@@ -0,0 +1,40 @@
+// 1. 만든다.
+// 2. 사용한다.
+// 3. 범위 정해서 내려준다.
+
+import { createContext, useContext, useState } from "react";
+
+interface ToDo {
+  id: number;
+  title: string;
+  content: string;
+  isComplete: boolean;
+}
+
+type ToDosContextValueType = {
+  toDos: ToDo[];
+  setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>;
+};
+
+const initialValue: ToDosContextValueType = {
+  toDos: [],
+  setToDos: () => {},
+};
+
+const ToDosContext = createContext(initialValue);
+
+export const useToDos = () => useContext(ToDosContext);
+
+interface ToDosProviderProps {
+  children: React.ReactNode;
+}
+
+export function ToDosProvider({ children }: ToDosProviderProps) {
+  const [toDos, setToDos] = useState<ToDo[]>([]);
+
+  return (
+    <ToDosContext.Provider value={{ toDos, setToDos }}>
+      {children}
+    </ToDosContext.Provider>
+  );
+}
